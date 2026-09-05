5b23d5f4606fa827a46c534c3e6e53902e7f8030
onefiter <onefiter@gmail.com>
2026-09-05 17:21:29 +0800

    css-loader style-loader

---
+++ Node/05_webpack/src/component/div_cpn.js
@@ -0,0 +1,8 @@
+import "../css/div_style.css"
+
+// 创建div
+const divE1 = document.createElement("div")
+divE1.textContent = "Hello World"
+divE1.classList.add("content")
+document.body.append(divE1)
+
---
+++ Node/05_webpack/wk.config.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/main.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "./build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                type: "javascript/auto"
+            },
+            {
+                // 告诉webpack匹配文件
+                test: /\.css$/,
+                use: [ // use中多个loader的使用顺序是从后向前的
+                    { loader: "style-loader" },
+                    { loader: "css-loader" }
+
+                ]
+            }
+        ]
+    }
+}
